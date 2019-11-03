5180ab0ff84089afdedf9c1af1082237ff9f8c5c
Austin WIley <wileybeer@gmail.com>
2019-11-03 10:17:00 -0700

    clear and reload page with new array

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -1,91 +1,101 @@
 const contactList = [{
-    name: "Austin Wiley",
-    phone: "123-123-1234"
-},
-{
-    name: "Sarah Wiley",
-    phone: "123-123-1234"
-},
-{
-    name: "Emmry Wiley",
-    phone: "123-123-1234"
-},
-{
-    name: "Haven Wiley",
-    phone: "123-123-1234"
-},
-{
-    name: "Ira Wiley",
-    phone: "123-123-1234"
-},
-{
-    name: "Austin Wiley",
-    phone: "123-123-1234"
-},
-{
-    name: "Chris christopherson",
-    phone: "123-123-1234"
-},
-{
-    name: "myly cyrus",
-    phone: "123-123-1234"
-},
-{
-    name: "poop plop",
-    phone: "123-123-1234"
-},
-{
-    name: "Soup eater",
-    phone: "123-123-1234"
-},
-{
-    name: "shelly mcmaster",
-    phone: "123-123-1234"
-},
-{
-    name: "fidor mangy",
-    phone: "123-123-1234"
-}
+        name: "Austin Wiley",
+        phone: "123-123-1234"
+    },
+    {
+        name: "Sarah Wiley",
+        phone: "123-123-1234"
+    },
+    {
+        name: "Emmry Wiley",
+        phone: "123-123-1234"
+    },
+    {
+        name: "Haven Wiley",
+        phone: "123-123-1234"
+    },
+    {
+        name: "Ira Wiley",
+        phone: "123-123-1234"
+    },
+    {
+        name: "Austin Wiley",
+        phone: "123-123-1234"
+    },
+    {
+        name: "Chris christopherson",
+        phone: "123-123-1234"
+    },
+    {
+        name: "myly cyrus",
+        phone: "123-123-1234"
+    },
+    {
+        name: "poop plop",
+        phone: "123-123-1234"
+    },
+    {
+        name: "Soup eater",
+        phone: "123-123-1234"
+    },
+    {
+        name: "shelly mcmaster",
+        phone: "123-123-1234"
+    },
+    {
+        name: "fidor mangy",
+        phone: "123-123-1234"
+    }
 
 ]
 
+// this loasds the initial list of contacts from the array of contacts
+function loadlist() {
+    var table = document.getElementById('tbody');
+    for (var i = 0; i < contactList.length; i++) {
+        var person = contactList[i];
+        var tr = document.createElement('tr');
 
-console.log(contactList[0])
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
 
+        var text1 = document.createTextNode(person.name);
+        var text2 = document.createTextNode(person.phone);
 
-var table = document.getElementById('tbody');
-for (var i = 0; i < contactList.length; i++) {
-var person = contactList[i];
-var tr = document.createElement('tr');
+        td1.appendChild(text1);
+        td2.appendChild(text2);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
 
-var td1 = document.createElement('td');
-var td2 = document.createElement('td');
-
-var text1 = document.createTextNode(person.name);
-var text2 = document.createTextNode(person.phone);
-
-td1.appendChild(text1);
-td2.appendChild(text2);
-tr.appendChild(td1);
-tr.appendChild(td2);
-
-table.appendChild(tr);
-}
-// document.tbody.appendChild(table);
-
-document.getElementById("button-addon2").addEventListener("click", displayDate);
-
-function displayDate() {
-//   document.getElementById("demo").innerHTML = Date();
-var newName = document.getElementById("nameInput").value
-var newPhone = document.getElementById("phoneInput").value
-console.log(newName)
-console.log(newPhone)
-var newEntry = {
-name: newName,
-phone: newPhone
+        table.appendChild(tr);
+    };
 };
 
-contactList.push(newEntry)
-console.log(contactList)
-}
+// This function gets the new contact input and pushes it into the contacts array
+function getContact() {
+    //   document.getElementById("demo").innerHTML = Date();
+    var newName = document.getElementById("nameInput").value
+    var newPhone = document.getElementById("phoneInput").value
+    console.log(newName)
+    console.log(newPhone)
+    var newEntry = {
+        name: newName,
+        phone: newPhone
+    };
+    contactList.push(newEntry)
+    console.log(contactList)
+};
+
+// THis funtion Empties the table so it can be rewritten
+function reloadList() {
+    const myNode = document.getElementById("tbody");
+    myNode.innerHTML = '';
+};
+
+//document.tbody.appendChild(table);
+
+//Event Listener for a click on the input button
+document.getElementById("button-addon2").addEventListener("click", getContact);
+
+//initally load the list when page is first loaded
+loadlist();
